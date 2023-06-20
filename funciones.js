5d31c603eb6bfbe1986cdda98daf1788205614a3
Daniela-Briceno <64908814+Daniela-Briceno@users.noreply.github.com>
2023-06-20 08:15:29 -0400

    Update funciones.js
    v.2

--- funciones.js
+++ funciones.js
@@ -1,17 +1,18 @@
 function calcularMM1(tasaLlegada, tasaAtencion) {
-    var Wq = (1 / (tasaAtencion - tasaLlegada)) * (1 / tasaAtencion);
-    var W = Wq + (1 / tasaAtencion);
-    var Lq = (tasaLlegada * tasaLlegada) / (tasaAtencion * (tasaAtencion - tasaLlegada));
-    var L = Lq + (tasaLlegada / tasaAtencion);
+    var p = tasaLlegada/tasaAtencion;
+    var Wq = tasaLlegada/(tasaAtencion*(tasaAtencion-tasaLlegada));
+    var W =1 / (tasaAtencion-tasaLlegada);
+    var Lq = ((tasaLlegada/tasaAtencion) * (tasaLlegada/tasaAtencion)) / (1 -(tasaLlegada/tasaAtencion));
+    var L = tasaLlegada / (tasaAtencion- tasaLlegada);
 
     res1.textContent = '';
     resultado.textContent = 'M/M/1';
-    res2.textContent = "\nTiempo promedio de espera en la cola (Wq): " + Wq + "\n";
-    res3.textContent = "\nTiempo promedio de espera en el sistema (W): " + W + "\n";
-    res4.textContent = "\nNúmero promedio de clientes en la cola (Lq): " + Lq + "\n";
-    res5.textContent = "\nNúmero promedio de clientes en el sistema (L): " + L;
+    res2.textContent = "\n(Wq)Tiempo promedio de espera en la cola (minutos): " + Wq + "\n";
+    res3.textContent = "\n(Ws)Tiempo promedio de espera en el sistema (minutos): " + W + "\n";
+    res4.textContent = "\n(Lq)Número promedio de clientes en la cola: " + Lq + "\n";
+    res5.textContent = "\n(Ls)Número promedio de clientes en el sistema: " + L;
 
-    return resultados;
+    return true;
 }
 
 function factorial(n) {
@@ -38,22 +39,22 @@ function calculateP0(tasaLlegada, tasaAtencion, servidores, rho) {
 }
 
 function calculateMMS(tasaLlegada, tasaAtencion, servidores) {
-    var rho = tasaLlegada / (servidores * tasaAtencion);
-    var P0 = calculateP0(tasaLlegada, tasaAtencion, servidores, rho);
-    var Wq = (P0 * Math.pow((tasaLlegada / tasaAtencion), servidores) * (servidores * Math.pow(rho, servidores))) / (factorial(servidores) * Math.pow(1 - rho, 2));
+    var p = tasaLlegada / (servidores*tasaAtencion);
+    var P0 = calculateP0(tasaLlegada, tasaAtencion, servidores, p);
+    var Lq = ((tasaAtencion*tasaLlegada)*(Math.pow(tasaLlegada/tasaAtencion,servidores))*P0) / ((factorial(servidores-1) * Math.pow(servidores*tasaAtencion-tasaLlegada, 2)));
+    var L = (((tasaAtencion*tasaLlegada)*(Math.pow(tasaLlegada/tasaAtencion,servidores)))/((factorial(servidores-1))*(Math.pow(servidores*tasaAtencion-tasaLlegada,2))))*P0 + (tasaLlegada/tasaAtencion);
+    var Wq = (tasaAtencion*(Math.pow(tasaLlegada/tasaAtencion,servidores))*P0)/((factorial(servidores-1))*(Math.pow(servidores*tasaAtencion-tasaLlegada,2)));
+    var W = (tasaAtencion*(Math.pow(tasaLlegada/tasaAtencion,servidores)))/((factorial(servidores-1))*(Math.pow(servidores*tasaAtencion-tasaLlegada,2)))*P0 +(1/tasaAtencion);
 
-    var W = Wq + (1 / tasaAtencion);
-    var Lq = (P0 * Math.pow((tasaLlegada / tasaAtencion), servidores) * (servidores * rho)) / (factorial(servidores) * Math.pow(1 - rho, 2));
-    var L = Lq + (tasaLlegada / tasaAtencion);
 
     resultado.textContent = 'M/M/s';
-    res1.textContent = "\nProbabilidad de que un cliente deba esperar en la cola (P0): " + P0 + "\n";
-    res2.textContent = "\nTiempo promedio de espera en la cola (Wq): " + Wq + "\n";
-    res3.textContent = "\nTiempo promedio de espera en el sistema (W): " + W + "\n";
-    res4.textContent = "\nNúmero promedio de clientes en la cola (Lq): " + Lq + "\n";
-    res5.textContent = "\nNúmero promedio de clientes en el sistema (L): " + L;
+    res1.textContent = "\n(P0)Probabilidad de que haya al menos un cliente en la cola : " + P0 + "\n";
+    res2.textContent = "\n(Wq)Tiempo promedio de espera en la cola (minutos): " + Wq + "\n";
+    res3.textContent = "\n(Ws)Tiempo promedio de espera en el sistema (minutos): " + W + "\n";
+    res4.textContent = "\n(Lq)Número promedio de clientes en la cola : " + Lq + "\n";
+    res5.textContent = "\n(Ls)Número promedio de clientes en el sistema : " + L;
 
-    return resultados;
+    return true;
 }
 
 var modeloSelect = document.getElementById("modelo");
@@ -64,30 +65,29 @@ var res2 = document.getElementById("res2");
 var res3 = document.getElementById("res3");
 var res4 = document.getElementById("res4");
 var res5 = document.getElementById("res5");
-
-
-
-
+var inputservidores = document.getElementById("servidores");
+inputservidores.disabled = true;
 
 modeloSelect.addEventListener("change", function() {
     var opcionSeleccionada = modeloSelect.value;
-
-    if (opcionSeleccionada === "mm1") {
+    if (opcionSeleccionada == "mm1" || opcionSeleccionada != "mms") {
+        inputservidores.disabled = true;
         calcularBtn.addEventListener("click", function() {
             var tasaLlegadaStr = document.getElementById("tasallegada").value;
             var tasaAtencionStr = document.getElementById("tasaatencion").value;
 
             var resultadosMM1 = calcularMM1(parseFloat(tasaLlegadaStr), parseFloat(tasaAtencionStr));
-            result.textContent = resultadosMM1;
+            //result.textContent = resultadosMM1;
         });
-    } else if (opcionSeleccionada === "mms") {
+    } else if (opcionSeleccionada == "mms") {
+        inputservidores.disabled = false;
         calcularBtn.addEventListener("click", function() {
             var tasaLlegadaStr = document.getElementById("tasallegada").value;
             var tasaAtencionStr = document.getElementById("tasaatencion").value;
             var servidoresStr = document.getElementById("servidores").value;
 
             var resultadosMMS = calculateMMS(parseFloat(tasaLlegadaStr), parseFloat(tasaAtencionStr), parseFloat(servidoresStr));
-            result.textContent = resultadosMMS;
+            //result.textContent = resultadosMMS;
         });
     }
 });
@@ -95,7 +95,6 @@ modeloSelect.addEventListener("change", function() {
 calcularBtn.addEventListener("click", function() {
     var tasaLlegadaStr = document.getElementById("tasallegada").value;
     var tasaAtencionStr = document.getElementById("tasaatencion").value;
-
     var resultadosMM1 = calcularMM1(parseFloat(tasaLlegadaStr), parseFloat(tasaAtencionStr));
-    result.textContent = resultadosMM1;
+    //result.textContent = resultadosMM1;
 });
